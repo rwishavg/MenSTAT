86bd78dd108f62342ffcca479433d4a95eca4a86
Rwishav <grwishav34@gmail.com>
2021-05-29 18:58:32 +0530

    Bug Fixes

--- src/script.js
+++ src/script.js
@@ -3,3 +3,9 @@ function storeName() {
     localStorage.setItem("uname", input);
 }
 document.getElementById("name").innerHTML = localStorage.getItem("uname");
+
+function storeEntry() {
+    var entry = document.getElementById("subject").value;
+    localStorage.setItem("journal_entry", entry);
+    alert(entry)
+}
