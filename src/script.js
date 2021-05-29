bdeff877df52e6727714ae53161a3a1ff8681c29
Rwishav <grwishav34@gmail.com>
2021-05-30 05:22:44 +0530

    bug fixes

--- src/script.js
+++ src/script.js
@@ -1,11 +1,18 @@
 function storeName() {
     var input = document.getElementById("username").value;
     localStorage.setItem("uname", input);
+    // alert(input)
+    const URL = 'URL'
+    const xhr = new XMLHttpRequest();
+    sender = input;
+    xhr.open('POST', URL);
+    xhr.send(sender);
+    location.href = '/src/journal.html';
 }
 document.getElementById("name").innerHTML = localStorage.getItem("uname");
 
 function storeEntry() {
     var entry = document.getElementById("subject").value;
     localStorage.setItem("journal_entry", entry);
-    alert(entry)
+    location.href = '/src/graph.html';
 }
