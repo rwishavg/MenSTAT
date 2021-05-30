352b279e11ec214c45885198189ac251b9ad661d
Rwishav <grwishav34@gmail.com>
2021-05-30 21:59:18 +0530

    paths updated in src

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ function storeName() {
     sender = input;
     xhr.open('POST', URL);
     xhr.send(sender);
-    location.href = '/src/journal.html';
+    location.href = 'MenSTAT/src/journal.html';
 }
 document.getElementById("name").innerHTML = localStorage.getItem("uname");
 
@@ -15,5 +15,5 @@ function storeEntry() {
     var entry = document.getElementById("subject").value;
     localStorage.setItem("journal_entry", entry);
     // location.href = '/src/graph.html';
-    location.href = '/src/graph.html';
+    location.href = 'MenSTAT/src/graph.html';
 }
